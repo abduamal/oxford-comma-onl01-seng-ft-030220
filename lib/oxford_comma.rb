b77156e994259dfc8206b2be6714b083a062bbc2
Amal Abdu <abduamal@ymail.com>
2020-03-06 21:33:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    strung = []
+    if array.length == 2
+        array.join(" and ")
+    elsif array.length > 2
+        new_array = array.insert(-2, "and")
+        new_array[0..-2] {|ele| new_array.join(", ") + new_array[-1]}
+    else
+        array.join
+    end
+    
 end
